d5727b35b495d1116d926f3ae08c94e83bc2a913
Nicolas Odermatt-Lemay <nodermattlemay@gmail.com>
2026-01-28 23:43:42 -0500

    Add caft-old/keycodes.h.

---
+++ keyboards/bastardkb/dilemma/3x5_2/assembled/keymaps/cafr-old/keycodes.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include QMK_KEYBOARD_H
+
+#include "quantum/quantum_keycodes.h"
+
+enum layers {
+    LAYER_BASE = 0,
+    LAYER_MOUSE,
+    LAYER_NAV,
+    LAYER_SYM,
+    LAYER_NUM,
+};
+
+/* enum custom_keycodes { */
+/*   SYM = QK_USER, */
+/*   E_GRV, */
+/*   A_GRV, */
+/*   U_GRV, */
+/*   E_CIRC, */
+/*   A_CIRC, */
+/*   I_CIRC, */
+/*   O_CIRC, */
+/*   U_CIRC, */
+/*   C_CEDL, */
+/*   E_DIAE, */
+/*   I_DIAE, */
+/*   M_PAREN, */
+/*   M_BRC, */
+/*   M_CBR, */
+/*   COMBO_TEST */
+/* }; */
+
+#define C_W     C(FR_W)
+#define C_S_TAB C(S(KC_TAB))
+#define C_TAB   C(KC_TAB)
+
+/* Left hand Mod-Tap */
+#define LGUI_A     LGUI_T(FR_A)
+#define LALT_R     LALT_T(FR_R)
+#define LCTL_S     LCTL_T(FR_S)
+#define LSFT__T    LSFT_T(FR_T)
+
+/* Right hand Mod-Tap */
+#define LSFT_N     LSFT_T(FR_N)
+#define LCTL_E     LCTL_T(FR_E)
+#define LALT_I     LALT_T(KC_I)
+#define LGUI_O     LGUI_T(KC_O)
+
+/* Layer-Tap */
+#define NAV_SPC  LT(LAYER_NAV,  KC_SPC)
+#define FOO_TAB  LT(LAYER_TODO, KC_TAB)
+#define SYM_ENT  LT(LAYER_SYM,  KC_ENT)
+#define NUM_E    LT(LAYER_NUM,  KC_E)
+
+#define MOU_TAB  LT(LAYER_MOUSE, KC_TAB)
+#define NUM_BSP  LT(LAYER_NUM,   KC_BSPC)
+
+/*One Shot Keys*/
+#define OSL_BAS  OSL(LAYER_BASE)
+#define OSL_SYM  OSL(LAYER_SYM)
+#define OSL_NUM  OSL(LAYER_NUM)
+
+/* Toggle layer */
+#define TO_BASE  TO(LAYER_BASE)
+#define TO_SYM   TO(LAYER_SYM)
+
+/* Test */
+#define FRC_U_GRAVE SEND_STRING("`u")
+
+/* Macros */
+// #define M_PAREN SEND_STRING("()" SS_TAP(X_LEFT))
