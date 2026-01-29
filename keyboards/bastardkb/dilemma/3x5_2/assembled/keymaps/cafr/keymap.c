241ba07bc41ba8dc0b2f7d3dc1743bb389638e55
Nicolas Odermatt-Lemay <nodermattlemay@gmail.com>
2026-01-28 23:42:01 -0500

    Caps Word: Don't shift KC_MINS ('-').

--- keyboards/bastardkb/dilemma/3x5_2/assembled/keymaps/cafr/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_2/assembled/keymaps/cafr/keymap.c
@@ -75,6 +75,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
+// This is the default callack except what I don't shift '-' to '_'.
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_MINS:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
+
 void custom_shift_key(uint8_t mod_state, uint16_t keycode, uint16_t shifted_keycode) {
     if (mod_state & MOD_MASK_SHIFT) {
         tap_code16(shifted_keycode);
